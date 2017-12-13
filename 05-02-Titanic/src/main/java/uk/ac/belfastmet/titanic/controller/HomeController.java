413abab23f33615c1a1f12715e769fbfb576bebd
BOY17149884 <BOY17149884@CLCLRCAC20.belfastmet.int>
2017-12-13 16:45:07 +0000

    Titanic passenger list working

--- 05-02-Titanic/src/main/java/uk/ac/belfastmet/titanic/controller/HomeController.java
+++ 05-02-Titanic/src/main/java/uk/ac/belfastmet/titanic/controller/HomeController.java
@@ -1,18 +1,41 @@
 package uk.ac.belfastmet.titanic.controller;
 
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import uk.ac.belfastmet.titanic.domain.Passenger;
+import uk.ac.belfastmet.titanic.respository.PassengerRepository;
+
 
 @Controller
 @RequestMapping("/")
 public class HomeController
 {
+	@Autowired
+	PassengerRepository passengerRepository;
+
+
+		
+		public HomeController(PassengerRepository passengerRepository) {
+		super();
+		this.passengerRepository = passengerRepository;
+	}
+
+
+
 	@GetMapping("/")
 	public String home(Model model)
 	{
+		model.addAttribute("pageTitle","Home");
+		model.addAttribute("passengers", passengerRepository.findAll());
 		return "homePage";
 	}
-
+		
 }
+
--- 05-02-Titanic/src/main/java/uk/ac/belfastmet/titanic/domain/Passenger.java
+++ 05-02-Titanic/src/main/java/uk/ac/belfastmet/titanic/domain/Passenger.java
@@ -8,141 +8,102 @@ import javax.persistence.Id;
 public class Passenger
 {
 
-	
-		public Passenger()
-		{
-			super();
-		}
+
 		
 		@Id
 		@GeneratedValue
 		
-		private Integer pasengerId; 
+		private Integer passengerid;
 		private Integer survived;
 		private Integer pclass;
 		private String name;
 		private String sex;
 		private Float age;
-		private Integer sibSp;
+		private Integer sibsp;
 		private Integer parch;
 		private String ticket;
 		private Float fare;
 		private String cabin;
 		private String embarked;
 		
-		public Passenger(Integer pasengerId, Integer survived, Integer pclass, String name, String sex, Float age, Integer sibSp, Integer parch, String ticket, Float fare, String cabin, String embarked)
-		{
+		
+		
+		public Passenger() {
 			super();
-			
-			this.pasengerId = pasengerId; 
-			this.survived = survived;
-			this.pclass = pclass;
-			this.name = name;
-			this.sex = sex;
-			this.age = age;
-			this.sibSp = sibSp;
-			this.parch = parch;
-			this.ticket = ticket;
-			this.fare = fare;
-			this.cabin = cabin;
-			this.embarked = embarked;
 		}
-
-
-		public Integer getPasengerId() {
-			return pasengerId;
+		public Integer getPassengerid() {
+			return passengerid;
 		}
-
-		public void setPasengerId(Integer pasengerId) {
-			this.pasengerId = pasengerId;
+		public void setPassengerid(Integer passengerid) {
+			this.passengerid = passengerid;
 		}
-
 		public Integer getSurvived() {
 			return survived;
 		}
-
 		public void setSurvived(Integer survived) {
 			this.survived = survived;
 		}
-
 		public Integer getPclass() {
 			return pclass;
 		}
-
 		public void setPclass(Integer pclass) {
 			this.pclass = pclass;
 		}
-
 		public String getName() {
 			return name;
 		}
-
 		public void setName(String name) {
 			this.name = name;
 		}
-
 		public String getSex() {
 			return sex;
 		}
-
 		public void setSex(String sex) {
 			this.sex = sex;
 		}
-
 		public Float getAge() {
 			return age;
 		}
-
 		public void setAge(Float age) {
 			this.age = age;
 		}
-
-		public Integer getSibSp() {
-			return sibSp;
+		public Integer getSibsp() {
+			return sibsp;
 		}
-
-		public void setSibSp(Integer sibSp) {
-			this.sibSp = sibSp;
+		public void setSibsp(Integer sibsp) {
+			this.sibsp = sibsp;
 		}
-
 		public Integer getParch() {
 			return parch;
 		}
-
 		public void setParch(Integer parch) {
 			this.parch = parch;
 		}
-
 		public String getTicket() {
 			return ticket;
 		}
-
 		public void setTicket(String ticket) {
 			this.ticket = ticket;
 		}
-
 		public Float getFare() {
 			return fare;
 		}
-
 		public void setFare(Float fare) {
 			this.fare = fare;
 		}
-
 		public String getCabin() {
 			return cabin;
 		}
-
 		public void setCabin(String cabin) {
 			this.cabin = cabin;
 		}
-
 		public String getEmbarked() {
 			return embarked;
 		}
-
 		public void setEmbarked(String embarked) {
 			this.embarked = embarked;
 		}
+		
+
 
 }//Class Passenger End

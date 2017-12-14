0e17501dfa7fd7beed9fcbbc25bf8222ea0322cd
BOY17149884 <BOY17149884@CLCLRCAC20.belfastmet.int>
2017-12-14 15:05:54 +0000

    titanic code, view works, other functions (search) almost done

--- 05-02-Titanic/src/main/java/uk/ac/belfastmet/titanic/controller/HomeController.java
+++ 05-02-Titanic/src/main/java/uk/ac/belfastmet/titanic/controller/HomeController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import uk.ac.belfastmet.titanic.domain.Passenger;
 import uk.ac.belfastmet.titanic.respository.PassengerRepository;
 
@@ -22,10 +24,11 @@ public class HomeController
 
 
 		
-		public HomeController(PassengerRepository passengerRepository) {
+		public HomeController(PassengerRepository passengerRepository)
+		{
 		super();
 		this.passengerRepository = passengerRepository;
-	}
+		}
 
 
 
@@ -37,5 +40,60 @@ public class HomeController
 		return "homePage";
 	}
 		
-}
+	@GetMapping("/passenger/view/{passengerId}")
+	public String view(@PathVariable("passengerId") Integer passengerId, Model model)
+	{
+		model.addAttribute("pageTitle","View");
+		model.addAttribute("passenger", passengerRepository.findOne(passengerId));
+		return "viewPage";
+	}
+	
+	@GetMapping("/passenger/edit/{passengerId}")
+	public String editPassenger(@PathVariable("passengerId") Integer passengerId, Model model)
+	{
+		model.addAttribute("pageTitle","Edit Paasenger");
+		model.addAttribute("passenger", passengerRepository.findOne(passengerId));
+		return "editPage";
+	}
+	
+	@GetMapping("/pasenger/delete/{passengerId}")
+	public String deletePassenger(@PathVariable("passengerId") Integer pasengerId)
+	{
+		passengerRepository.delete(passengerId);
+		return "redirect:/";
+	}
+	
+	
+	@PostMapping("/passengers/search")
+	public String search(@RequestParam("searchField") String searchField, 
+	
+	@RequestParam("searchString") String searchString, Model model) 
+	{
+		if(searchField.equals("name"))
+		{		
+			model.addAttribute("pageTitle","Search Passengers");
+			model.addAttribute("passengers", passengerRepository.findByName("%" + searchString + "%"));
+			
+		}
+			else if(searchField.equals("embarked"))
+			{
+				model.addAttribute("pageTitle","Search Passengers");
+				model.addAttribute("passengers", passengerRepository.findByEmbarked(searchString));
+			}
+				else if (searchField.equals("passsengerClass"))
+				{
+					Integer pClass = Integer.parseInt(searchString);
+					model.addAttribute("pageTitle","Search Passengers");
+					model.addAttribute("passengers", passengerRepository.findByPclass(pClass));
+				}
+			
+			return "passengerPage";
+	}
+		
+		
 
+
+
+
+	
+}
--- 05-02-Titanic/src/main/java/uk/ac/belfastmet/titanic/respository/PassengerRepository.java
+++ 05-02-Titanic/src/main/java/uk/ac/belfastmet/titanic/respository/PassengerRepository.java
@@ -6,4 +6,30 @@ import uk.ac.belfastmet.titanic.domain.Passenger;
 
 public interface PassengerRepository extends CrudRepository<Passenger, Integer> 
 {
+	
+	
+//	<!-- Need code for sarch --> 
+	
+	
+	
+	
+	
+	Iterable<Passenger> findByPclass(int i);
+	
+	Iterable<Passenger> findBySurvived(int i);
+	
+	Iterable<Passenger> findByEmbarked(String searchField);
+	
+	Iterable<Passenger> findByName(String searchField);
+	
+	Iterable<Passenger> findByNameLike(String searchField);
+	
+	
+	
+	
+	
+	
+	//Iterable<Passenger> findBySurvived 
+	
+	
 }
